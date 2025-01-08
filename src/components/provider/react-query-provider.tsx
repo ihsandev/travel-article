febded84ab72cee4e73989e0864590369f06f1e7
ihsandev <ihksansanhas@gmail.com>
2025-01-08 21:24:55 +0800

    init: react query

---
+++ src/components/provider/react-query-provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function ReactQueryProvider({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
